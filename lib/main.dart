0495daaf5653b653c677dbc9c5e56a86a98f3d74
Abdelrahman Hussein <elhussieny100@gmail.com>
2023-08-20 01:44:31 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:itistore/views/screens/welcome_screen.dart';
 import 'package:sizer/sizer.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
